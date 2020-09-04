8d99312f3d898e0ea22ff4d4bd684c4265639a98
Sidewinder1138 <andersonlambda@gmail.com>
2020-09-04 15:55:17 -0700

    cleanup

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,5 @@
-import * as _ from "lodash";
-import { printMe } from './print';
 import { Main } from "./Main";
 import { Box } from "./Box";
-
-// function component() {
-//   const element = document.createElement('div');
-//   const btn = document.createElement('button');
-//   element.innerHTML = _.join(['Hello', 'webpack', 'chris'], ' ');
-//   btn.innerHTML = 'Click me and check the console!';
-//   btn.onclick = printMe;
-//   element.appendChild(btn);
-//   return element;
-// }
-// document.body.appendChild(component());
 
 const main = new Main();
 const box = new Box({
